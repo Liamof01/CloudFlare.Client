6acb843ce1b8d0fe5520915d8ec21510e59bc4d1
Liam O'Flynn <liamo@cclonline.com>
2023-12-08 00:55:58 +0000

    Created new LoadBalancer class

---
+++ CloudFlare.Client/Api/Zones/LoadBalancer/LoadBalancer.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace CloudFlare.Client.Api.Zones.LoadBalancer
+{
+    /// <summary>
+    /// Load balancer
+    /// </summary>
+    public class LoadBalancer
+    {
+        /// <summary>
+        /// Load balancer identifier
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Name of the record
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Time to live (TTL) of the DNS entry for the IP address returned by this load balancer.
+        /// This only applies to gray-clouded (unproxied) load balancers.
+        /// </summary>
+        [JsonProperty("ttl")]
+        public int? Ttl { get; set; }
+    }
+}
